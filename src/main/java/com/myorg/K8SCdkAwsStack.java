73d44b9c1b2f342bcbcc12e0ce1d551d38d7827c
vladimir seprak <vladimir.seprak@gmail.com>
2022-02-02 15:12:53 +0100

    k8s stack

--- src/main/java/com/myorg/K8SCdkAwsStack.java
+++ src/main/java/com/myorg/K8SCdkAwsStack.java
@@ -11,9 +11,11 @@ import software.amazon.awscdk.CfnOutput;
 import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.services.ec2.ISubnet;
 import software.amazon.awscdk.services.ec2.SubnetConfiguration;
+import software.amazon.awscdk.services.ec2.SubnetSelection;
 import software.amazon.awscdk.services.ec2.SubnetType;
 import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.eks.Cluster;
+import software.amazon.awscdk.services.eks.KubernetesVersion;
 import software.amazon.awscdk.services.sqs.Queue;
 
 public class K8SCdkAwsStack extends Stack {
@@ -30,13 +32,15 @@ public class K8SCdkAwsStack extends Stack {
 		final Queue queue = Queue.Builder.create(this, "K8SCdkAwsQueue").visibilityTimeout(Duration.seconds(300))
 				.build();
 		
-        final Cluster cluster = Cluster.Builder.create(this, "ecs-cluster")
+        Vpc vpcStack = getVPCStack(this, "my-test-eks-vpc");
+		final Cluster cluster = Cluster.Builder.create(this, "ecs-cluster")
                 .clusterName("my-test-cluster")
-                .vpc(getVPCStack(this, "app-name", "my-test-eks-vpc"))
+                .vpcSubnets(List.of(SubnetSelection.builder().subnets(vpcStack.getPrivateSubnets()).build()))
+                .vpc(vpcStack).version(KubernetesVersion.V1_21)
             .build();
 	}
 	
-    public Vpc getVPCStack(final Construct scope, final String appName, String vpcName) {
+    public Vpc getVPCStack(final Construct scope, String vpcName) {
         Vpc vpc = Vpc.Builder.create(this, "vpc")
                 .cidr("10.0.0.0/16")
                 .enableDnsHostnames(true)
@@ -44,10 +48,10 @@ public class K8SCdkAwsStack extends Stack {
                 .maxAzs(2)
                 .subnetConfiguration(
                         List.of(
-                            SubnetConfiguration.builder()
-                        .name("public")
-                        .subnetType(SubnetType.PUBLIC)
-                                .build()))
+                            SubnetConfiguration.builder().name("my-private-1").subnetType(SubnetType.PRIVATE_ISOLATED).build(),
+                            SubnetConfiguration.builder().name("my-private-2").subnetType(SubnetType.PRIVATE_ISOLATED).build(),
+                            SubnetConfiguration.builder().name("my-public-1").subnetType(SubnetType.PUBLIC).build(),
+                            SubnetConfiguration.builder().name("my-public-2").subnetType(SubnetType.PUBLIC).build()))
                 .build();
         Tags.of(vpc).add("Name", vpcName);
         Tags.of(vpc).add("Purpose", "training");
